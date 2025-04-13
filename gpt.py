25930058e2883d6c991dc2caf3a84a9f0a412ae9
blindTissue <skim434@jh.edu>
2025-04-13 17:05:17 -0400

    updated until masking

--- gpt.py
+++ gpt.py
@@ -8,7 +8,7 @@ block_size = 256 # what is the maximum context length for predictions?
 max_iters = 5000
 eval_interval = 500
 learning_rate = 3e-4
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = 'cuda' if torch.cuda.is_available() else 'mps'
 eval_iters = 200
 n_embd = 384
 n_head = 6
@@ -106,12 +106,29 @@ class MultiHeadAttention(nn.Module):
 class FeedFoward(nn.Module):
     """ a simple linear layer followed by a non-linearity """
 
-    def __init__(self, n_embd):
+    def __init__(self, n_embd, linear_bias=True, activation='relu', scaleup=4):
         super().__init__()
+
+        # You can add more activation functions here
+        if activation == 'relu':
+            activation = nn.ReLU()
+        elif activation == 'gelu':
+            activation = nn.GELU()
+        elif activation == 'silu':
+            activation = nn.SiLU()
+        elif activation == 'glu':
+            activation = nn.GLU()
+        if linear_bias:
+            scale_up_ffn = nn.Linear(n_embd, scaleup * n_embd)
+            scale_down_ffn = nn.Linear(scaleup * n_embd, n_embd)
+        else:
+            scale_up_ffn = nn.Linear(n_embd, scaleup * n_embd, bias=False)
+            scale_down_ffn = nn.Linear(scaleup * n_embd, n_embd, bias=False)
+
         self.net = nn.Sequential(
-            nn.Linear(n_embd, 4 * n_embd),
-            nn.ReLU(),
-            nn.Linear(4 * n_embd, n_embd),
+            scale_up_ffn,
+            activation,
+            scale_down_ffn,
             nn.Dropout(dropout),
         )
 
@@ -121,18 +138,31 @@ class FeedFoward(nn.Module):
 class Block(nn.Module):
     """ Transformer block: communication followed by computation """
 
-    def __init__(self, n_embd, n_head):
+    def __init__(self, n_embd, n_head, norm_type='ln', norm='pre', parallel_ffn=False):
         # n_embd: embedding dimension, n_head: the number of heads we'd like
         super().__init__()
         head_size = n_embd // n_head
         self.sa = MultiHeadAttention(n_head, head_size)
         self.ffwd = FeedFoward(n_embd)
-        self.ln1 = nn.LayerNorm(n_embd)
-        self.ln2 = nn.LayerNorm(n_embd)
+        self.norm = norm
+        self.parallel_ffn = parallel_ffn
+        if norm_type == 'ln':
+            self.ln1 = nn.LayerNorm(n_embd)
+            self.ln2 = nn.LayerNorm(n_embd)
+        elif norm_type == 'rms':
+            self.ln1 = nn.RMSNorm(n_embd)
+            self.ln2 = nn.RMSNorm(n_embd)
 
     def forward(self, x):
-        x = x + self.sa(self.ln1(x))
-        x = x + self.ffwd(self.ln2(x))
+        if self.parallel_ffn:
+            x = x + self.ffwd(self.ln2(x)) + self.sa(self.ln1(x))
+        else:
+            if self.norm == 'pre':
+                x = x + self.sa(self.ln1(x))
+                x = x + self.ffwd(self.ln2(x))
+            else:
+                x = self.ln1(x + self.sa(x))
+                x = self.ln2(x + self.ffwd(x))
         return x
 
 class GPTLanguageModel(nn.Module):
